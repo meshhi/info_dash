6e03f86217ed81cc5d912a614ca242edd193a256
meshhi <yumythug@yandex.ru>
2024-02-08 14:51:39 +0300

    slides

--- src/components/Slides/Questions.tsx
+++ src/components/Slides/Questions.tsx
@@ -2,6 +2,11 @@ import { FC, PropsWithChildren } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import bg_about_models from "/src/assets/about_models/bg_about_models.svg"
+import bg_what_can from "/src/assets/what_can_do/bg_what_can.png"
+import questions_chat from "/src/assets/questions/questions_chat.png"
+import questions_doc from "/src/assets/questions/questions_doc.png"
+import questions_stepic from "/src/assets/questions/questions_stepic.png"
+import questions_tg from "/src/assets/questions/questions_tg.png"
 
 const StyledContainer = styled(motion.section)`
     width: 100%;
@@ -33,39 +38,92 @@ const TextContent = styled.p`
     margin-bottom: 55px;
 `
 
-const Cards = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-`
-
-const Card = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+const Grid = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
     gap: 36px;
-    max-width: 478px;
+    margin-bottom: 40px;
 `
 
-const CardImg = styled.img`
-    width: 478px;
-    height: 314px;
+const GridItem = styled.div`
+    display: flex;
+    border: 2px solid #48D6E5;
     border-radius: 15px;
-    background-color: grey;
+    background-color: #EDF3FA;
+    background-color: white;
+    gap: 20px;
 `
-const CardText = styled.p`
+
+const GridImageBg = styled.div`
+    background-image: url(${!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? bg_what_can
+    : "bg_what_can.png"});
+    min-width: 211px;
+    min-height: 166px;
+    background-color: #E3EAEF;
+    flex-basis: 48%;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 8px;
+`
+
+const GridImage = styled.img`
+    width: 60%;
+    height: 60%;
+`
+
+const GridText = styled.p`
     font-size: 20px;
     font-weight: 300;
     text-align: start;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 const Comment = styled.div`
-
+    border: 2px solid #48D6E5;
+    border-radius: 15px;
+    background-color: #EDF3FA;
+    background-color: white;
+    padding: 29px 54px;
+    font-size: 20px;
+    font-weight: 300;
 `
 
 export const Questions: FC<PropsWithChildren> = () => {
-    
+    const gridContent = [
+        {
+            src: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+            ? questions_chat
+            : "questions_chat.png",
+            text: () => <h1>У нас есть сообщество в Телеграм. Многие пользователи отмечают,<br></br> что на любой вопрос получают оперативный\n ответ. 
+            t.me/awcommunity – присоединяйтесь. Спрашивайте и отвечайте!</h1>
+        },
+        // {
+        //     src: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+        //     ? questions_doc
+        //     : "questions_doc.png",
+        //     text: `У системы есть документация с примерами и картинками – если будут вопросы “как это работает”, то в ней точно есть ответы
+        //     Документация`
+        // },
+        // {
+        //     src: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+        //     ? questions_stepic
+        //     : "questions_stepic.png",
+        //     text: "У нас есть бесплатный курс на Stepik – “BI-аналитик”Возможно, вы с него и пришли, чтобы практику делать. Если нет – то скорее запишитесь, там мы учим навыкам работы BI-аналитика. "
+        // },
+        // {
+        //     src: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+        //     ? questions_tg
+        //     : "questions_tg.png",
+        //     text: "У нас есть форум с описанием конкретных примеров, там же вы можете оставить пожелания по развитию функциональности системы – мы их ценим и стараемся учитывать в будущих релизах."
+        // }
+    ]
+
     return (
         <StyledContainer
             variants={{
@@ -85,9 +143,16 @@ export const Questions: FC<PropsWithChildren> = () => {
             animate="final"
         >
             <Header>Где задавать вопросы и учиться?</Header>
-            <Cards>
-
-            </Cards>
+            <Grid>
+                {
+                    gridContent.map(item => <GridItem key={item.text}>
+                        <GridImageBg>
+                            <GridImage src={item.src}></GridImage>
+                        </GridImageBg>
+                        <GridText>{item.text()}</GridText>
+                    </GridItem>)
+                }
+            </Grid>
             <Comment>
             А ещё есть телеграм-канал для развития насмотренности и навыков в Data Driven – Data Driven культура (ссылка: https://t.me/awbi_ru). Подписывайтесь.
             </Comment>
